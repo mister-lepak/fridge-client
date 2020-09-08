93e3cddee6c7916e85ee80a797dd55d7ccd6ac6e
Try Wijono <3wijono@gmail.com>
2020-09-08 23:32:48 +0800

    first commit

---
+++ precache-manifest.aa63651fcf3e9829de74bc6875e0a567.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbf177e39045b60130205a0a644754d8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0c5fef971cc36eead139",
+    "url": "./static/js/2.dbcc2724.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "./static/js/2.dbcc2724.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b395db3971919ac2c97f",
+    "url": "./static/js/main.ac3aba91.chunk.js"
+  },
+  {
+    "revision": "42df955b8c70bf8a6d54",
+    "url": "./static/js/runtime-main.9d156cb0.js"
+  }
+]);
